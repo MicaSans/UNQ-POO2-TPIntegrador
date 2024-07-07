7b0d104e64f5caa8c7649b45a717e8270e117845
Matias Bauhoffer <mati.bauhoffer@gmail.com>
2024-07-06 22:37:31 -0300

    Agrego test para Estacionamiento y agrego variables y métodos en Celular

--- src/ar/edu/unq/tp/integrador/Celular.java
+++ src/ar/edu/unq/tp/integrador/Celular.java
@@ -1,10 +1,42 @@
 package ar.edu.unq.tp.integrador;
 
-public class Celular {
+import java.util.Random;
 
+public class Celular {
+	String nroCelular;
+	String prefijoCelular = "15";
+	int saldo;
+	
+	public Celular() {
+		this.nroCelular = generarNroDeCelular();
+		this.saldo = 0;
+	}
+	
 	public int getSaldo() {
-		return 0;
+		return this.saldo;
 		
 	}
+	
+	private String generarNroDeCelular() {
+		Random random = new Random();
 
+		// Genera un número aleatorio de 8 dígitos
+		int randomCelular = 10000000 + random.nextInt(90000000);
+
+		// Concatenar "11" al inicio del número aleatorio generado
+		return this.getPrefijoCelular() + randomCelular;
+	}
+	
+	public String getPrefijoCelular() {
+		return this.prefijoCelular;
+	}
+	
+	public String getNroCelular() {
+		return this.nroCelular;
+	}
+
+	public void cargarSaldo(int montoCarga) {
+		this.saldo += montoCarga;
+	}
+	
 }
