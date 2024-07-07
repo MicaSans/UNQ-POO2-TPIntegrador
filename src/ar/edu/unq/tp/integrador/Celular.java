ab2d8a2fad103ed741433dd45c199ec9f790333d
Matias Bauhoffer <mati.bauhoffer@gmail.com>
2024-07-06 23:08:55 -0300

    Borro unos comments en Celular

--- src/ar/edu/unq/tp/integrador/Celular.java
+++ src/ar/edu/unq/tp/integrador/Celular.java
@@ -19,11 +19,8 @@ public class Celular {
 	
 	private String generarNroDeCelular() {
 		Random random = new Random();
-
-		// Genera un número aleatorio de 8 dígitos
 		int randomCelular = 10000000 + random.nextInt(90000000);
 
-		// Concatenar "11" al inicio del número aleatorio generado
 		return this.getPrefijoCelular() + randomCelular;
 	}
 	
