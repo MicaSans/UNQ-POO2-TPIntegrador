c124c53089e34960283c11ffa76e602d93654613
Matias Bauhoffer <mati.bauhoffer@gmail.com>
2024-07-07 22:17:46 -0300

    Agrego test doubles a los test de Estacionamiento y renombro el file.

--- test/ar/edu/unq/tp/integrador/EstacionamientoTest.java
+++ test/ar/edu/unq/tp/integrador/EstacionamientoTest.java
@@ -6,23 +6,29 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
 
-class testEstacionamiento {
+class EstacionamientoTest {
+	Celular celular;
+	
+	@BeforeEach
+	public void setUp() {
+		celular = mock(Celular.class);
+	}
 
 	@Test
 	void testEstaVigenteEstacionamientoApp() {
-		Celular celular = new Celular();
 		LocalDateTime horaInicio = LocalDateTime.of(LocalDate.now(), LocalTime.of(8, 0));
 		Estacionamiento estacionamiento = new EstacionamientoApp(celular, "DDD999", horaInicio);
-		celular.cargarSaldo(600);
+		when(celular.getSaldo()).thenReturn(600);
 		
 		assertTrue(estacionamiento.estaVigente());
 	}
 	
 	@Test
 	void testNoEstaVigenteEstacionamientoApp() {
-		Celular celular = new Celular();
 		LocalDateTime horaInicio = LocalDateTime.of(LocalDate.now(), LocalTime.of(8, 0));
 		Estacionamiento estacionamiento = new EstacionamientoApp(celular, "DDD999", horaInicio);
 		celular.cargarSaldo(40); //le alcanzaría solo para 1 hora por lo que podría pagar hasta las 9 a.m.
